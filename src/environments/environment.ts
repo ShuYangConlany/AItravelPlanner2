910c7762cf674c010f2a3ecc59acc77773ff1dab
ShuYang <yangshu2022@yahoo.com>
2024-05-09 17:27:39 -0400

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://aitravelplannerbackend-1e9dfaf62568.herokuapp.com'
-    apiUrl: 'http://localhost:8080'
+    apiUrl: 'https://aitravelplannerbackend-1e9dfaf62568.herokuapp.com'
+    // apiUrl: 'http://localhost:8080'
 
   };
   
